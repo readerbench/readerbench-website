a665731a07b4ca9dfa2865335f42fcaeb7c020bf
Gabi Gutu <gabi.gutu@gmail.com>
2016-01-15 19:25:37 +0200

    Concept map bug

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -421,7 +421,7 @@
 							if($scope.topicEdges.count == response.data.data.concepts.links.count)
 							{
 								clearInterval(interval);
-								d3jsForTopics(response.data.data, ".conceptMap");
+								d3jsForTopics(response.data.data.concepts, ".conceptMap");
 							}
 				        }, 1000);
 						//console.log($scope.topics);
