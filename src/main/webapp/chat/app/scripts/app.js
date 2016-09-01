c2bd6d8d900e8d5e66054fac8fcf46aaf6e851b3
Stefan <sbrighiu@gmail.com>
2016-09-01 11:21:46 +0300

    - added example bubblechart - works all

--- src/main/webapp/chat/app/scripts/app.js
+++ src/main/webapp/chat/app/scripts/app.js
@@ -33,6 +33,7 @@ angular.module('chat', ['ngModules', '3rdPartyModules', 'controllers', 'models',
  * Get default language and use it for the translation service
  */
 .config(['$translateProvider', 'configProvider', function ($translateProvider, configProvider) {
+
     $translateProvider.useStaticFilesLoader({
         prefix: '/chat/app/lang/',
         suffix: '.json'
@@ -43,7 +44,14 @@ angular.module('chat', ['ngModules', '3rdPartyModules', 'controllers', 'models',
 .config(['configProvider', 'SocketProvider', function (configProvider, SocketProvider) {
     SocketProvider.connect(configProvider.config.socket);
 }])
-.run(['$rootScope', 'Socket', 'config', 'Auth', '$translate', function ($rootScope, Socket, config, Auth, $translate) {
+.run(['$rootScope', 'Socket', 'config', 'Auth', '$translate', '$templateCache', function ($rootScope, Socket, config, Auth, $translate, $templateCache) {
+
+    $templateCache.put('template/modal/window.html', "<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\"><div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-lg': size == 'lg'}\"><div class=\"modal-content\" ng-transclude></div></div></div>");
+
+    $templateCache.put('template/modal/backdrop.html', "<div class=\"modal-backdrop fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1040 + (index && 1 || 0) + index*10}\"></div>");
+
+    $templateCache.put('template/tooltip/tooltip-popup.html', "<div class=\"tooltip {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\"><div class=\"tooltip-arrow\"></div><div class=\"tooltip-inner\" ng-bind=\"content\"></div></div>");
+
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
         //you code goes here
         console.log(toParams);
