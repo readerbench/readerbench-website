d77209a4f0d22be0766887e570772fecab342bc4
gabi.gutu <gabi.gutu@gmail.com>
2016-09-20 16:35:41 +0300

    Demo Menu changed design

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -95,7 +95,7 @@ var AboutSections;
             .controller('DemoMenuController', ['$location', '$scope', function ($location, $scope) {
                     $scope.demoItems = DemoItems;
                     $scope.getClass = function (path) {
-                        return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+                        return ($location.path().substr(0, path.length) === path) ? 'current' : '';
                     }
                 }])
             .controller('DemoController',
