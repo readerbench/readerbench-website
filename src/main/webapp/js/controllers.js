df1a6e41b0aada3b519448a9c05156ef5cc3568e
gabi.gutu <gabi.gutu@gmail.com>
2016-10-06 12:01:33 +0300

    SentimentAnalysis - added word granularity

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -255,6 +255,17 @@ var AboutSections;
                                                                 sentenceColorRGB.b
                                                             );
                                                             jQuery('.results-sentiment #paragraph-' + i + ' #sentence-' + j + ' span.sentence').css('background-color', sentenceColorHex);
+                                                            
+                                                            for (var k = 0; k < sentence.innerObjects.length; k++) {
+                                                                var word = sentence.innerObjects[k];
+                                                                var wordColorRGB = computeColors(word, 0.1);
+                                                                var wordColorHex = rgbToHex(
+                                                                    wordColorRGB.r,
+                                                                    wordColorRGB.g,
+                                                                    wordColorRGB.b
+                                                                );
+                                                                jQuery('.results-sentiment #paragraph-' + i + ' #sentence-' + j + ' #word-' + k + ' span.word').css('background-color', wordColorHex);
+                                                            }
                                                         }
                                                     }
                                                 }
