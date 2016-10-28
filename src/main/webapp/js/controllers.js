76a4c0efdc36e52273a433911885997ff4c2f523
Gabi Gutu <gabi.gutu@gmail.com>
2016-10-28 11:50:01 +0300

    Minnor CV fixes (II)

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1370,7 +1370,8 @@ var AboutSections;
                                                                                                                                 return;
                                                                                                                             }
                                                                                                                             
-                                                                                                                            $scope.text = '<p>' + response.data.data.text.replace(/\n/g, "</p><p>") + '</p>';
+                                                                                                                            if (typeof response.data.data.text !== 'undefined')
+                                                                                                                                $scope.text = '<p>' + response.data.data.text.replace(/\n/g, "</p><p>") + '</p>';
 
                                                                                                                             // build concept map
                                                                                                                             $scope.showConceptMap = true;
