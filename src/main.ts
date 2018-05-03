33d809a130bef3cf3cc4d1746b70bf224393c9f9
Gabi Gutu <gabi.gutu@gmail.com>
2018-05-03 14:06:52 +0300

    Stop removing trailing Slash

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Location } from '@angular/common';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const __stripTrailingSlash = (Location as any).stripTrailingSlash;
+(Location as any).stripTrailingSlash = function _stripTrailingSlash(url: string): string {
+  return /[^\/]\/$/.test(url) ? url : __stripTrailingSlash(url);
+}
 
 if (environment.production) {
   enableProdMode();
