7f4dd89e1177c75f18cb493fec1b6ed6b1fba8f3
Gabi Gutu <gabi.gutu@gmail.com>
2018-05-14 17:21:01 +0300

    Basic functionalities for Community Demo

--- src/app/pages/demo/community/community.module.ts
+++ src/app/pages/demo/community/community.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommunityComponent } from './community.component';
+import { ParticipantComponent } from './participant/participant.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CommunityComponent]
+  declarations: [CommunityComponent, ParticipantComponent, CarouselComponent, SlideComponent]
 })
 export class CommunityModule { }
---
+++ src/app/pages/demo/community/participant/participant.do.ts
@@ -0,0 +1,29 @@
+import { ReaderbenchService } from "../../../../readerbench.service";
+import { DemoDO } from "../../demo.do";
+
+export class ParticipantDO extends DemoDO {
+    participantName: string;
+    participantNickname: string;
+    Contrib: number;
+    Scr: number;
+    SocialKB: number;
+    InterAnim: number;
+    InDegree: number;
+    OutDegree: number;
+    group: string;
+
+    protected getPrimitivePropertyKeys(): string[] {
+        return ["participantName", "participantNickname", "Contrib", "Scr", "SocialKB", "InterAnim", "InDegree", "OutDegree", "group"];
+    }
+
+    public buildFromObject(object: Object) {
+		super.buildFromObject(object);
+        var rbUtils = new ReaderbenchService();
+        
+        this.Scr = rbUtils.roundNumberToTwoDecimals(this.Scr);
+        this.SocialKB = rbUtils.roundNumberToTwoDecimals(this.SocialKB);
+        this.InterAnim = rbUtils.roundNumberToTwoDecimals(this.InterAnim);
+        this.InDegree = rbUtils.roundNumberToTwoDecimals(this.InDegree);
+        this.OutDegree = rbUtils.roundNumberToTwoDecimals(this.OutDegree);
+    }
+}
