6d9d9cdbc6587c2f911309448ad42c08c3edef2d
Gabi Gutu <gabi.gutu@gmail.com>
2018-05-25 16:29:17 +0300

    Updated Projects section

--- src/app/pages/projects/projects.data.ts
+++ src/app/pages/projects/projects.data.ts
@@ -1,6 +1,38 @@
 export var Projects = [
     {
-        title: 'ENeA SEA',
+        title: 'Robots and Society: Cognitive Systems for Personal Robots and Autonomous Vehicles (Robin)',
+        link: '#',
+        description: 'User-centered project designing software and services modules for the use of robots in an interconnected digital society that enables companies to develop complex, intelligent and performing products and services for these users as well as society as a whole.',
+        category: 'Information and Communication Technologies',
+        image: 'assets/images/robin_picture.jpg.jpg',
+        imageDescription: 'Use of robots in an interconnected digital society'
+    },
+    {
+        title: 'Virtual Library of Romanian Literature: systematization and digitization (Intellit)',
+        link: '#',
+        description: 'Development of automated software tools for semantic search and analysis in the Romanian literary heritage based on advanced Natural Language Processing techniques that facilitate the analysis of large volumes of text.',
+        category: 'Information and Communication Technologies',
+        image: 'assets/images/intellit_picture.jpg',
+        imageDescription: 'Semantic search and analysis in the Romanian literary heritage'
+    },
+    {
+        title: 'Empowering elderly people in conversations with professionals (PrepDOC)',
+        link: '#',
+        description: 'Application for elderly people to prepare themselves for conversations with healthcare professionals, so that they arrive at a consult with a better description of their situation, and trust in the professional. Bring the application to the market (healthcare practices, nursing homes).',
+        category: 'Information and Communication Technologies',
+        image: 'assets/images/prepdoc_picture.jpg',
+        imageDescription: 'Elderly people can prepare themselves for conversations with healthcare professionals'
+    },
+    {
+        title: 'Revitalizing libraries and cultural heritage through advanced technologies (Lib2Life)',
+        link: '#',
+        description: 'Implements a mechanism for indexing and searching for relevant documents based on keywords and providing documents similar to a given document.',
+        category: 'Information and Communication Technologies',
+        image: 'assets/images/lib2life_picture.jpg',
+        imageDescription: 'Relevant documents are provided based on keywords and through analyses of other documents'
+    },
+    {
+        title: 'Early Nutrition eAcademy Southeast Asia (ENeA SEA)',
         link: 'https://www.enea-sea.eu/',
         description: 'ENeA SEA develops innovative e-learning for practicing doctors in the field of early nutrition in Southeast Asia. EU, Thai and Malaysian partners comprised of universities/institutions and professional organizations in nutrition are jointly developing science-based education, tailored to the needs of the Southeast Asian region. The goal of the project is to offer the latest recommendations for everyday practice and maximise outreach to the wider healthcare community.',
         category: 'Erasmus+ Capacity-Building',
@@ -8,7 +40,15 @@ export var Projects = [
         imageDescription: 'Innovative e-learning for practicing doctors in the field of early nutrition in Southeast Asia'
     },
     {
-        title: 'RAGE',
+        title: 'Innovative technology hub based on semantic models and high performance computing (HUB-TECH)',
+        link: '#',
+        description: 'HUB-TECH is an innovative technology hub that aggregates technological and human resources to facilitate the process of writing research topics/optimized technical solutions by semantically recommending relevant textual resources to be consulted (for example, scientific papers, doctoral theses, patents, results of RDI projects, etc.), introduction of mechanisms for partial generation of documentation, and creation of a discussion forum to provide personalized support for the marketing of products and services.',
+        category: 'Information and Communication Technologies',
+        image: 'assets/images/hub_screenshot.png',
+        imageDescription: 'Relevant textual resources to be consulted (scientific papers, doctoral theses, patents, etc.) to facilitate the process of writing research topics'
+    },
+    {
+        title: 'Realising an Applied Gaming Ecosystem (RAGE)',
         link: 'http://rageproject.eu/',
         description: 'RAGE aims to develop, transform and enrich advanced technologies from the leisure games industry into self-contained gaming assets (i.e. solutions showing economic value potential) that support game studios at developing applied games easier, faster and more cost-effectively. These assets will be available along with a large volume of high-quality knowledge resources through a self-sustainable Ecosystem, which is a social space that connects research, gaming industries, intermediaries, education providers, policy makers and end-users. RAGE - Realising an Applied Gaming Eco-system, is a 48-months Technology and Know-How driven Research and Innovation project co-funded by EU Framework Programme for Research and Innovation, Horizon 2020.',
         category: 'Horizon 2020',
@@ -16,7 +56,7 @@ export var Projects = [
         imageDescription: 'Delivering access to advanced gaming technology resources and state-of-the-art knowledge to develop Applied Games easier, faster and more cost-effectively'
     },
     {
-        title: 'Language Technologies for Lifelong Learning',
+        title: 'Language Technologies for Lifelong Learning (LTfLL)',
         link: 'http://www.ltfll-project.org/',
         description: 'The outcomes of the LTfLL project are prototypes of next-generation services built on advanced research on the application of language technologies in education. Their exploitation to a consumer market requires some further development (on usability, transferability to other domains and other learning environments, etc.).The target groups (as targeted people) of the LTfLL outcomes can be grouped in three: Researchers and developers, Content service providers and End-users of the LTfLL services. The LTfLL project is partially supported/co-funded by the European Union under the Information and Communication Technologies (ICT) theme of the 7th Framework Programme for R&D (FP7-ICT-2007-1-4.1)',
         category: 'Information and Communication Technologies',
