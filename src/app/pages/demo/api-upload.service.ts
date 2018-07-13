4261ae1bf3a99cd98082777ead75af445418e53c
Gabi Gutu <gabi.gutu@gmail.com>
2018-07-13 17:59:41 +0300

    API endpoint fix

--- src/app/pages/demo/api-upload.service.ts
+++ src/app/pages/demo/api-upload.service.ts
@@ -10,8 +10,8 @@ export class ApiUploadService {
 
   constructor(private http: Http) { }
 
-  private url = '//localhost:8080/api/';
-  // private url = '//readerbench.com/api/';
+//   private url = '//localhost:8080/api/';
+  private url = '//readerbench.com/api/';
   private endpoint: string;
   private headers: Headers;
   private formData: FormData;
