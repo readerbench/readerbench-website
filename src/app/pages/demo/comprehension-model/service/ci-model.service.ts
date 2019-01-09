1cc1e1dfa0c9387bc9d263845a3e9f5e937259a4
Ionut Cristian Paraschiv <ionut@3angle.tech>
2019-01-09 14:39:39 +0200

    Fixed issues w/ CIModelService

--- src/app/pages/demo/comprehension-model/service/ci-model.service.ts
+++ src/app/pages/demo/comprehension-model/service/ci-model.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 
 import { CMResult } from './data-objects/cm-result.do';
 import { ApiRequestService } from '../../api-request.service';
-import { ApiResponseModel } from '../../api-response.model';
 
 export interface CiModelParams {
   text: string;
@@ -14,18 +14,21 @@ export interface CiModelParams {
 @Injectable()
 export class CIModelService {
 
-  constructor(private apiRequestService: ApiRequestService) {
+  constructor(
+    private apiRequestService: ApiRequestService,
+  ) {
+    this.apiRequestService.setHeaders(this.apiRequestService.HEADERS_TYPE_COMMON_REQUEST);
     this.apiRequestService.setApiService("ciModelAnalyzer");
   }
 
   public getWords(params: CiModelParams): Observable<CMResult> {
     var process = this.apiRequestService.process(params);
-    process.subscribe(response => {
-      const cmResult = new CMResult();
-      cmResult.buildFromObject(response.data);
-      return cmResult;
-    });
-    return null; // fix me
+    return process
+      .pipe(map(response => {
+        const cmResult = new CMResult();
+        cmResult.buildFromObject(response.data);
+        return cmResult;
+      }));
   }
 
 }
