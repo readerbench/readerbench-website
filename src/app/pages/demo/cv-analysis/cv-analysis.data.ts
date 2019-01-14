df937fd25ad8ce4087416cd257ead09cfe1cfe99
Gabi Gutu <gabi.gutu@gmail.com>
2019-01-14 13:03:26 +0200

    #34 Fixed CV Analysis bugs and errors (2)

--- src/app/pages/demo/cv-analysis/cv-analysis.data.ts
+++ src/app/pages/demo/cv-analysis/cv-analysis.data.ts
@@ -1,9 +1,12 @@
-import { DefaultInputData } from "../demo.component.data";
+import { DefaultInputData } from '../demo.component.data';
 
-export var CvAnalysisData = {
+export let CvAnalysisData = {
     'serviceName': 'cvAnalysis',
     'componentTitle': 'Curriculum Vitae Analysis',
-    'keywords': 'prospection, prospect, développement, clients, fidélisation, chiffred’affaires, marge, vente, portefeuille, négociation, budget, rendez-vous, proposition, terrain, téléphone, rentabilité, business, reporting, veille, secteur, objectifs, comptes, animation, suivi, création, gestion',
+    'keywords': 'prospection, prospect, développement, clients, fidélisation, chiffred’affaires, marge, ' +
+        'vente, portefeuille, négociation, budget, rendez - vous, proposition, terrain, téléphone, ' +
+        'rentabilité, business, reporting, veille, secteur, objectifs, comptes, animation, suivi, création, ' +
+        'gestion',
     'ignore': 'janvier, février, mars, avril, mai, juin, juillet, août, septembre, octobre, novembre, décembre',
     'languages': [
         DefaultInputData.languages[1]
