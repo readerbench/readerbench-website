8e9654a17f80265ce5999b71426d1400f43f995a
Gabi Gutu <gabi.gutu@gmail.com>
2019-01-14 10:27:29 +0200

    #34 Fixed CV Analysis bugs and errors

--- src/app/pages/demo/cv-analysis/cv-analysis.module.ts
+++ src/app/pages/demo/cv-analysis/cv-analysis.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CvAnalysisComponent } from './cv-analysis.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CvAnalysisComponent]
+  declarations: []
 })
 export class CvAnalysisModule { }
