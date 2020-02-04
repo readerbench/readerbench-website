73348f01d35fdc0ac6ab7b7cb95450d10a338ff4
Gabi Gutu <gabi.gutu@gmail.com>
2020-02-04 16:12:40 +0200

    Added language for AMOC

---
+++ src/app/pages/demo/comprehension-model/comprehension-model.data.ts
@@ -0,0 +1,8 @@
+import { DefaultInputData } from '../demo.component.data';
+
+export let ComprehensionModelData = {
+    'languages': [
+        DefaultInputData.languages[0],  // en
+    ],
+    'defaultLanguage': DefaultInputData.languages[0] // en
+};
