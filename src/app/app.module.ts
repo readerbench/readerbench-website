d8173fa375b0590c47bf40f89d8853dae92052d1
Gabi Gutu <gabi.gutu@gmail.com>
2020-02-06 17:48:25 +0200

    Removed double declaration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,13 +28,7 @@ import { CsclComponent } from './pages/demo/cscl/cscl.component';
 import { CvAnalysisComponent } from './pages/demo/cv-analysis/cv-analysis.component';
 import { LakComponent } from './pages/demo/lak/lak.component';
 import { SemDiffComponent } from './pages/demo/sem-diff/sem-diff.component';
-import { ParticipantComponent } from './pages/demo/community/participant/participant.component';
-import { ParticipantEvolutionComponent } from './pages/demo/community/participant/participant-evolution/participant-evolution.component';
-import { CommunityCarouselComponent } from './pages/demo/community/carousel/community-carousel.component';
-import { SlideComponent } from './pages/demo/community/slide/slide.component';
 import { CommunityKeywordsHeatmapComponent } from './pages/demo/community/keywords-heatmap/keywords-heatmap.component';
-import { ClusteredForceLayoutComponent } from './pages/demo/community/clustered-force-layout/clustered-force-layout.component';
-import { MultiLevelEdgeBundlingComponent } from './pages/demo/community/multi-level-edge-bundling/multi-level-edge-bundling.component';
 
 
 import { DemoServicesComponent } from './pages/demo/sections/services/services.component';
@@ -53,13 +47,9 @@ import { ApproximationPipe } from './pipes/approximation.pipe';
 import { ConfigService } from './config/config.service';
 import { ApiRequestService } from './pages/demo/api-request.service';
 import { ReaderBenchService } from './readerbench.service';
-import { EdgeBundlingService } from './pages/demo/comprehension-model/service/edge-bundling.service';
 import { ConfigComponent } from './config/config.component';
-import { HierarchicalEdgeBundlingComponent } from './hierarchical-edge-bundling/hierarchical-edge-bundling.component';
-import { ChordComponent } from './pages/demo/community/chord-diagram/chord-diagram';
 import { KeywordsHeatmapComponent } from './pages/demo/keywords-heatmap/keywords-heatmap.component';
 import {CurriculumRecommendationComponent} from './pages/demo/curriculum-recomandation/curriculum-recommendation.component';
-import { DocumentAnalysisComponent } from './pages/demo/document-analysis/document-analysis';
 import { Ng5SliderModule } from 'ng5-slider';
 
 @NgModule({
@@ -102,15 +92,6 @@ import { Ng5SliderModule } from 'ng5-slider';
     ProjectsComponent,
     PublicationsComponent,
     ContactComponent,
-    ParticipantComponent,
-    ParticipantEvolutionComponent,
-    CommunityCarouselComponent,
-    SlideComponent,
-    HierarchicalEdgeBundlingComponent,
-    ClusteredForceLayoutComponent,
-    MultiLevelEdgeBundlingComponent,
-    ChordComponent,
-    DocumentAnalysisComponent
   ],
   imports: [
     BrowserModule,
