52be5a88277b2add9f8ab43c57dbec9140e95dd8
Gabi Gutu <gabi.gutu@gmail.com>
2020-12-06 19:02:24 +0200

    Added ATES logo and sdescription:

--- src/app/pages/projects/projects.data.ts
+++ src/app/pages/projects/projects.data.ts
@@ -1,5 +1,13 @@
 export var Projects = [
     {
+        title: 'Automated Text Evaluation and Simplification',
+        link: '#',
+        description: 'The general objective of the ATES (Automated Text Evaluation Simplification) project is to enable writers to improve the quality of their writing, in both English and Romanian languages. Writers will receive immediate feedback when typing the input text through personalized recommendations and highlights of different text elements that can be improved with an easy-to-use and intuitive user interface. The system will automatically evaluate the texts and provide guidance and instructions to allow users to adjust them accordingly to the targeted audience (e.g., belletristic, scientific writing styles), through text adjustments and simplification mechanisms. Our aim is to increase the reading ease of resulted materials and to incrementally refine the user’s writing style, transcending towards a higher writing proficiency. This objective will consider multiple experiments performed towards assessing text difficulty, before and after any automated adjustments are made.',
+        category: 'Physical Sciences and Engineering',
+        image: 'assets/images/ates_picture.jpg',
+        imageDescription: 'Automated text evaluation for tutors and learners'
+    },
+    {
         title: 'Robots and Society: Cognitive Systems for Personal Robots and Autonomous Vehicles (Robin)',
         link: '#',
         description: 'User-centered project designing software and services modules for the use of robots in an interconnected digital society that enables companies to develop complex, intelligent and performing products and services for these users as well as society as a whole.',
